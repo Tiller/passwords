93c0ddfc743fbf357037b6067d1df9e9f382279f
tiller <git.me@tiller.fr>
2022-11-04 22:49:54 +0100

    update to latest android

--- android_src/app/src/main/java/com/stooit/securepasswordkeyboard/KeyboardViewFix.java
+++ android_src/app/src/main/java/com/stooit/securepasswordkeyboard/KeyboardViewFix.java
@@ -9,27 +9,16 @@ import android.util.AttributeSet;
 public class KeyboardViewFix extends KeyboardView {
     public static boolean inEditMode = true;
 
-    @TargetApi(21) // Build.VERSION_CODES.L
+    @TargetApi(33) // Build.VERSION_CODES.L
     public KeyboardViewFix(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(inEditMode ? new ContextWrapperInner(context) : context, attrs, defStyleAttr, defStyleRes);
+        super(context, attrs, defStyleAttr, defStyleRes);
     }
 
     public KeyboardViewFix(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(inEditMode ? new ContextWrapperInner(context) : context, attrs, defStyleAttr);
+        super(context, attrs, defStyleAttr);
     }
 
     public KeyboardViewFix(Context context, AttributeSet attrs) {
-        super(inEditMode ? new ContextWrapperInner(context) : context, attrs);
-    }
-
-    public static class ContextWrapperInner extends ContextWrapper {
-        Context base;
-        public ContextWrapperInner(Context base) {
-            super(base);
-            this.base = base;
-        }
-        public Object getSystemService(String name) {
-            return Context.AUDIO_SERVICE.equals(name) ? null : base.getSystemService(name);
-        }
+        super(context, attrs);
     }
 }
